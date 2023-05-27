2171ddcb0cbc064d79a312e808a27bc2e1024eab
Y_Salon03 <Y_Salon03@pc.az>
2023-05-27 16:20:24 +0400

    Add menu and try-catch

--- C#/Calculator App/Calculator App/Program.cs
+++ C#/Calculator App/Calculator App/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    try
+    {
+        Console.Write("Enter the first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Clear();
+        Console.WriteLine(@"Press '1' for plus (+)
+Press '2' for minus (-)
+Press '3' for multiplication (*)
+Press '4' for division (/)");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
