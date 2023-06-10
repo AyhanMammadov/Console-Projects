a82914334336061fa3e225f13965137974ec949e
Y_Salon03 <Y_Salon03@pc.az>
2023-06-10 16:04:02 +0400

    Add Calculator App

---
+++ C#/Calculator App/Calculator App/Program.cs
@@ -0,0 +1,52 @@
+﻿using Calculator_App;
+
+bool isover = true;
+while (isover)
+{
+    try
+    {
+        Console.Write("Enter the first number: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = double.Parse(Console.ReadLine());
+        Console.Clear();
+
+
+        Console.WriteLine(@"Press '1' for plus (+)
+Press '2' for minus (-)
+Press '3' for multiplication (*)
+Press '4' for division (/)
+Press 'e' for Exit (Close App)");
+
+
+        string userinput = Console.ReadLine();
+        Console.Clear();
+        switch (userinput)
+        {
+            case "1":
+                Calculator.Plus(num1, num2);
+                break;
+            case "2":
+                Calculator.Minus(num1, num2);
+                break;
+            case "3":
+                Calculator.Multiply(num1, num2);
+                break;
+            case "4":
+                Calculator.Divide(num1, num2);
+                break;
+            case "e":
+                Console.WriteLine("Thanx for using this app!");
+                isover = false;  
+                break;
+            default:
+                Console.WriteLine("Wrong input!");
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
